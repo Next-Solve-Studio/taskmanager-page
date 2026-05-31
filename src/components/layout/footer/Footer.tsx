77e3f49e5418a4258617e4ccad16caa058f44c2b
Wendell Eduardo Albuquerque Bonucci <contact.wendellbonucci@gmail.com>
2026-05-31 14:42:23 -0300

    Ajuste - Footer

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -16,7 +16,7 @@ type SocialItem = LinkItem & {
 export default function Footer() {
     const institutional: LinkItem[] = [
         { id: 1, label: "Corporativo", href: "/" },
-        { id: 2, label: "Contato", href: "/" },
+        { id: 2, label: "Contato", href: "/Contact  " },
         { id: 3, label: "Política de Privacidade", href: "/" },
         { id: 4, label: "Agente de IA", href: "/" },
     ];
